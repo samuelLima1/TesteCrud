610eeee234ecfb28296a59d1d9a510d6da40dcad
samuelLima1 <39566741+samuelLima1@users.noreply.github.com>
2018-07-08 16:47:34 -0400

    Create cadastro.php
    Help me to work! Please.

---
+++ cadastro.php
@@ -0,0 +1,36 @@
+<?php
+
+  include "conexao.php"
+  
+?>
+
+<html lang="pt-br">
+
+<head> 
+<title> Criando CRUD para avaliação </title>
+</head>
+<body>
+
+  <h1>Sistema de Escola</h2>
+  
+  <form method="POST" action="pagehome.php">
+  
+    <label>Nome Completo:</label>
+    <label>Data de Nascimento:</label>
+    <label>Logradouro:</label>
+    <label>Numero:</label>
+    <label>Bairro:</label>
+    <label>Cidade:</label>
+    <label>Cidade:</label>
+    <label>Estado:</label>
+    <label>Data de Criação:</label>
+
+</body>
+
+</html>
+
+<?php
+
+mysqli_close($con);
+
+?>
