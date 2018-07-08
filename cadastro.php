421c048d389d74f04a8bd18430135476eb229b85
samuelLima1 <39566741+samuelLima1@users.noreply.github.com>
2018-07-08 17:02:52 -0400

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -11,10 +11,11 @@
 </head>
 <body>
 
-  <h1>Sistema de Escola</h2>
+  <h1>Aluno:</h1>
   
   <form method="POST" action="pagehome.php">
-  
+  <div>
+    <label>ID do Aluno:</label>
     <label>Nome Completo:</label>
     <label>Data de Nascimento:</label>
     <label>Logradouro:</label>
@@ -23,8 +24,21 @@
     <label>Cidade:</label>
     <label>Cidade:</label>
     <label>Estado:</label>
+    <label>Cep:</label>
     <label>Data de Criação:</label>
-
+    <label>ID do Curso:</label>
+  </div>
+    <br/><br/>
+  
+    <h1>Curso:</h1>
+    
+    <form method="POST" action="pagehome.php">
+  <div>
+    <label>ID do Curso:</label>
+    <label>Nome do Curso:</label>
+    <label>Data de Criação:</label>
+    <label>ID do Professor:</label>
+  </div>  
 </body>
 
 </html>
